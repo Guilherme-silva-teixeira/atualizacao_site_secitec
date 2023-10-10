cb328bac4ee8e3e36d5c88e349c9fe16ed98586e
ShakeDev00 <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2023-10-10 08:27:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let corAtualMais1 = 1+1;
+const cores = ['color1', 'color2', 'color3', 'color4', 'color5','color6','color7', 'color8', 'color9', 'colora', 'colorb','colorc'];
+
+function mudar_cor_Background() {
+    document.body.classList.remove(cores[corAtualMais1]);
+    corAtualMais1 = (corAtualMais1 + 1) % cores.length;
+    document.body.classList.add(cores[corAtualMais1]);
+}
